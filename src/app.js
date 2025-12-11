6af1cb8bc4b7da3e36a8eb69fc3506994cea92c9
Devasenapathi <devahari1204@gmail.com>
2025-12-11 17:53:36 +0530

    dist file is updated

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import path from 'path';
 import swaggerUi from 'swagger-ui-express';
 import routes from './routes/index.js';
 import { errorHandler, notFoundHandler } from './middlewares/error.middleware.js';
 import swaggerSpec from './docs/swagger.js';
 
 const app = express();
+const distPath = path.resolve(process.cwd(), 'src', 'dist');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(express.static(distPath));
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 app.get('/docs.json', (req, res) => res.json(swaggerSpec));
 
 app.use('/api', routes);
+
+app.get('*', (req, res, next) => {
+	if (req.method !== 'GET' || req.path.startsWith('/api') || req.path.startsWith('/docs')) {
+		return next();
+	}
+	return res.sendFile(path.join(distPath, 'index.html'));
+});
 
 app.use(notFoundHandler);
 app.use(errorHandler);
